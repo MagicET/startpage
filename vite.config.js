6d0c4853e35ebb4fcbb900a7fc897c9f5d096f24
Magicet <60283888+MagicET@users.noreply.github.com>
2024-08-28 16:24:10 +0000

    デプロイできるようにした？

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+    assetsDir: './',
+    publicPath: './',
+  },
   plugins: [
     vue(),
   ],
